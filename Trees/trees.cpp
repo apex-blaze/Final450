616807ce065ce00f67f90cd9018f7e19a110393e
apex-blaze <ritish619@gmail.com>
2021-04-19 14:04:09 +0530

    added level order traversal

--- Trees/trees.cpp
+++ Trees/trees.cpp
@@ -14,6 +14,30 @@ struct Node{
     }
 };
 
+// Q.1 Level Order Traversal O(n)
+
+void levelOrder(Node* root){
+    if(root == NULL) 
+     return;
+    queue<Node*> q;
+    q.push(root); q.push(NULL);
+    while(!q.empty()){
+        Node* node = q.front();
+        q.pop();
+        if(node != NULL){
+            cout<<node->data<<" ";
+            if(node->left)
+                q.push(node->left);
+            
+            if(node->right)
+                q.push(node->right);
+            
+        }else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+    cout<<"\n";
+}
 
 int main(){
 
