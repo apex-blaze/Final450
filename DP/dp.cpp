b73702e6d083cf1f04890bef2ca7d6a847c4d705
apex-blaze <ritish619@gmail.com>
2021-07-06 15:02:10 +0530

    DP Problem 1

--- DP/dp.cpp
+++ DP/dp.cpp
@@ -2,6 +2,26 @@
 #include<climits>
 
 using namespace std;
+// Q.1 Coin Change Problem
+long long coinChange(int arr[],int n,int sum){
+	long long t[n+1][sum+1];
+
+	for(int j=0;j<sum+1;j++)	
+		t[0][j] = 0;
+	for(int i=0;i<n;i++)
+		t[i][0] = 1;
+	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<sum+1;j++){
+			if(arr[i-1] <= j){
+				t[i][j] = t[i-1][j] + t[i][j-arr[i-1]];
+			}else{
+				t[i][j] = t[i-1][j];
+			}
+		}
+	}
+	return t[n][sum];
+}
 
 // Q.2  0-1 Knapsack
 // Bottom Up Approach
@@ -22,6 +42,7 @@ using namespace std;
        }
        return t[n][W];
     }
+
 
 // Q.8 Equal Partition Sum
 
